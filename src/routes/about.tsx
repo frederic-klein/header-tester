804989a370b350f3e0c5e1471b5e35923fb64c29
frederic-klein <frederic.klein@rwth-aachen.de>
2023-02-24 17:31:01 +0100

    more headers

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -5,7 +5,10 @@ export default function Home() {
   return (
     <main>
       <HttpHeader name="Referrer-Policy" value="same-origin" />
-      <HttpHeader name="Custom-Header" value="Blubb" />
+      <HttpHeader name="Content-Security-Policy" value="self" />
+      <HttpHeader name="X-Frame-Options" value="SAMEORIGIN" />
+      <HttpHeader name="X-Content-Type-Options" value="nosniff" />
+      <HttpHeader name="Permissions-Policy" value="geolocation=(), microphone=()" />
       <Title>Hello World</Title>
       <h1>About</h1>
     </main>
