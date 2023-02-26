468485fc6731e627eb3f9ee3f79d62d006af94a4
frederic-klein <frederic.klein@rwth-aachen.de>
2023-02-26 22:24:41 +0100

    swtiched to no-referrer-when-downgrade

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -4,7 +4,7 @@ import { HttpHeader } from "solid-start/server";
 export default function Home() {
   return (
     <main>
-      <HttpHeader name="Referrer-Policy" value="same-origin" />
+      <HttpHeader name="Referrer-Policy" value="no-referrer-when-downgrade" />
       <HttpHeader name="Content-Security-Policy" value="self" />
       <HttpHeader name="X-Frame-Options" value="SAMEORIGIN" />
       <HttpHeader name="X-Content-Type-Options" value="nosniff" />
